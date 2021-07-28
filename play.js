bc19ef5ba709aaaf9daa199cb5e676679c4364b4
yhupaul <yhupaul@hotmail.com>
2021-07-28 14:14:33 -0700

    learn .write()

--- play.js
+++ play.js
@@ -11,8 +11,11 @@ const connect = function () {
   conn.setEncoding("utf8");
   
   conn.on("connect", () => {
-    console.log("event happen!!");
+    console.log("Successfully connected to game server");
+    conn.write("Name: YHU");
   });
+  
+  
 
   conn.on("data", (data) => {
     console.log(data);
