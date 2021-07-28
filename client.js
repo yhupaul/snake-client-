590c3d87347a988f4f6b33fc7ae4f385714fa97a
yhupaul <yhupaul@hotmail.com>
2021-07-28 13:39:11 -0700

    create client file

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',// IP address here,
+    port: 50542// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+    console.log("event happen!!");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  
+  return conn;
+};
+
+module.exports = connect; //{connect,};  if there are more than one valable
