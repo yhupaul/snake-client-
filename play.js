9cc39080eb4e534e6ff428f9c2b5dff993d2238d
yhupaul <yhupaul@hotmail.com>
2021-07-28 18:01:41 -0700

    setupIup and handleUser..

--- play.js
+++ play.js
@@ -2,7 +2,7 @@ const { Socket } = require("dgram");
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '135.23.223.133',// IP address here,
     port: 50542// PORT number here,
@@ -10,20 +10,36 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: YHU");
-    
+
   });
-  
+
 
   conn.on("data", (data) => {
     console.log(data);
   });
-  
+
   return conn;
 };
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function(input) {
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
 console.log("Connecting ...");
 connect();
